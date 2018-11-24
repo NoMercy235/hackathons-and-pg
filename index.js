7def71e7a859bbb661aa527a0cef39cba5caf03a
Alexandru Florian Barascu <alex.florin2352@gmail.com>
2018-11-24 16:53:55 +0200

    Fixed an issue where the paraphraser was not initializing correctly after the first post.

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ import { applyGlobalCSS, removeGlobalCSS } from './global-style';
 
 class Paraphraser {
   constructor (options) {
+    this._init(options);
+  }
+
+  _init (options) {
     this.options = options || this._getDefaultParams();
     this.listeners = [];
     this.isOpen = false;
     this.userAgreed = false;
-
-    this.targetArea = document.querySelector(this.options.textSelector);
-    console.log('Caught text element:', this.targetArea);
-
-    this.postBtn = document.querySelector(this.options.postBtnSelector);
-    console.log('Caught post button:', this.postBtn);
+    this.isApplied = false;
   }
 
   _getDefaultParams () {
@@ -29,6 +28,11 @@ class Paraphraser {
     this.isOpen = false;
     const node = document.getElementById('noho-paraphraser');
     node && body.removeChild(node);
+  }
+
+  _onContinue () {
+    this.postBtn.click();
+    this.remove();
   }
 
   _insertTextReplaceOptionsContainer (coords = { x: 0, y: 0 }) {
@@ -54,8 +58,10 @@ class Paraphraser {
 
     options.forEach(option => {
       const optionNode = createOption(option, selection => {
-        console.log('selected: ', selection);
-        document.querySelector('[data-editor]').innerHTML = selection;
+        this._onClose();
+        this._onClose();
+        this._removeHighlightOffensiveArea();
+        this.targetArea.innerHTML = selection;
       });
       container.appendChild(optionNode);
     });
@@ -63,10 +69,19 @@ class Paraphraser {
     const okWithIt = createOption('I don\t care. Let me post.', () => {
       this.userAgreed = true;
       this._onClose();
-      document.querySelector(this.options.postBtnSelector).click();
+      this._removeHighlightOffensiveArea();
+      this._onContinue();
     });
 
     container.appendChild(okWithIt);
+  }
+
+  _highlightOffensiveArea () {
+    document.querySelector(this.options.textSelector).style.border = '1px solid red';
+  }
+
+  _removeHighlightOffensiveArea () {
+    document.querySelector(this.options.textSelector).style.border = 'none';
   }
 
   _applyAreaListener () {
@@ -74,10 +89,10 @@ class Paraphraser {
     l = document.addEventListener('click', this._onClose.bind(this));
     this.listeners.push({ type: 'keydown', listener: l });
 
-    l = document.addEventListener('keydown', ev => {
-      const keyName = ev.key;
-      console.log('keydown event key: ' + keyName);
-    });
+    // l = document.addEventListener('keydown', ev => {
+    //   const keyName = ev.key;
+    //   console.log('keydown event key: ' + keyName);
+    // });
     this.listeners.push({ type: 'keydown', listener: l });
     console.log('Applied area event');
   }
@@ -86,12 +101,20 @@ class Paraphraser {
     console.log('Applied post btn event');
     const l = this.postBtn.addEventListener('click', async ev => {
       if (this.userAgreed) return;
+
       ev.stopPropagation();
 
-      const popupLoc = { x: +ev.pageX - (magic.CONTAINER_WIDTH / 2), y: +ev.pageY + magic.TOP_OFFSET };
-      console.log(`Tried to post. CLicked on coords (${popupLoc.x}, ${popupLoc.y})`);
-      this._insertTextReplaceOptionsContainer(popupLoc);
       const result = await getReplaceText(document.querySelector(this.options.textSelector).innerHTML);
+      if (!result.length) {
+        this._onContinue();
+        return;
+      }
+
+      const popupLoc = { x: +ev.pageX - (magic.CONTAINER_WIDTH / 2), y: +ev.pageY + magic.TOP_OFFSET };
+      console.log(`Tried to post. Clicked on coords (${popupLoc.x}, ${popupLoc.y})`);
+
+      this._highlightOffensiveArea();
+      this._insertTextReplaceOptionsContainer(popupLoc);
       this._insetTextOptions(result);
     });
     this.listeners.push({ type: 'keydown', listener: l });
@@ -99,19 +122,45 @@ class Paraphraser {
 
   apply () {
     console.group('Applying paraphraser');
+    this.isApplied = true;
+
+    this.targetArea = document.querySelector(this.options.textSelector);
+    console.log('Caught text element:', this.targetArea);
+
+    this.postBtn = document.querySelector(this.options.postBtnSelector);
+    console.log('Caught post button:', this.postBtn);
+
     applyGlobalCSS();
     this._applyAreaListener();
     this._applyPostBtnListener();
     console.groupEnd();
   }
 
-  reset () {
+  remove () {
+    console.log('Removing');
+    this.isApplied = false;
     this.listeners.forEach(l => {
       document.removeEventListener(l.type, l.listener);
     });
     removeGlobalCSS();
+    this._init();
   }
 }
 
 const para = new Paraphraser();
-para.apply();
+
+const checkExist = setInterval(() => {
+  if (para.isApplied) {
+    // Hack to check if the found button is actually the real one.
+    const postBtn = document.querySelector(para.options.postBtnSelector);
+    if (postBtn !== para.postBtn) { para.remove() }
+    return;
+  }
+
+  const text = document.querySelector(para.options.textSelector);
+  const postBtn = document.querySelector(para.options.postBtnSelector);
+
+  if (text && postBtn) {
+    para.apply();
+  }
+}, 1000);
